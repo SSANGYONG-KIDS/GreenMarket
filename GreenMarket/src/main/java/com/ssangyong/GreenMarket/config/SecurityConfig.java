d4acd4d7647adbdc9c42ebd2516fcfb883a0a88f
ino5 <chero77@naver.com>
2021-12-01 10:17:20 +0900

    자동 로그인 기능 구현

--- GreenMarket/src/main/java/com/ssangyong/GreenMarket/config/SecurityConfig.java
+++ GreenMarket/src/main/java/com/ssangyong/GreenMarket/config/SecurityConfig.java
@@ -4,6 +4,8 @@ package com.ssangyong.GreenMarket.config;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.ssangyong.GreenMarket.service.LoginService;
@@ -26,8 +30,17 @@ import lombok.extern.java.Log;
 public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 	
 	@Autowired
-	 LoginService loginService;
-	
+	LoginService loginService;
+	@Autowired
+	private DataSource dataSource;
+
+    // 자동로그인 - tokenRepository 구현
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
+    }	
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -66,7 +79,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 		.and()
 			.csrf().disable() //csrf(크로스사이트 위조요청에 대한 설정) 토큰 비활성화 (test시에는 disable권장) 
 			.exceptionHandling().accessDeniedPage("/login/accessDenied");// 403 예외처리 핸들링   권한이 없는 대상이 접속을시도했을 때
-         
+		
+		// 자동 로그인
+		http.rememberMe()
+				.userDetailsService(loginService)
+				.tokenRepository(tokenRepository());
 	}
 	
 	@Override
