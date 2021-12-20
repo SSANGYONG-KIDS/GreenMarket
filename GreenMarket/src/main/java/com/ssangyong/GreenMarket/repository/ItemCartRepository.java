4fc8fe14ed0d2502873f9cb7e564f609201f3d41
ino5 <chero77@naver.com>
2021-12-21 04:39:56 +0900

    Update the query of allFindByMid method in ItemCartRepository class

--- GreenMarket/src/main/java/com/ssangyong/GreenMarket/repository/ItemCartRepository.java
+++ GreenMarket/src/main/java/com/ssangyong/GreenMarket/repository/ItemCartRepository.java
@@ -11,6 +11,6 @@ import com.ssangyong.GreenMarket.model.ItemCartEntityId;
 
 
 public interface ItemCartRepository  extends CrudRepository<ItemCartEntity, ItemCartEntityId>, QuerydslPredicateExecutor<ItemCartEntity> {
-	@Query(value="SELECT it.* FROM itemcart ic left outer join item it ON (ic.i_id = it.i_id) WHERE ic.m_id=?1", nativeQuery = true)
+	@Query(value="SELECT it.i_id, it.i_content, it.i_price, it.i_regdate, it.i_state, it.i_title, it.i_tstate, it.m_id, it.i_category FROM itemcart ic left outer join item it ON (ic.i_id = it.i_id) WHERE ic.m_id=?1", nativeQuery = true)
 	public List<Object[]> allFindByMId(String mId);
 }
